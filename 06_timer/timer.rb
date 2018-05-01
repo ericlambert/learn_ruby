7339482bac5072d30037f8f1aff28b9f5272fb18
Eric Lambert <ericlamberttwo@gmail.com>
2018-05-01 13:40:07 -0700

    Finished

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,24 @@
 class Timer
-  #write your code here
+	def initialize
+		@seconds = 0
+	end
+
+	def seconds=(num)
+		@seconds = num
+	end
+
+	def seconds
+		@seconds
+	end
+
+	def time_string
+		minutes = seconds / 60
+		hours = minutes / 60
+		self.seconds = seconds % 60
+		minutes = minutes % 60
+
+		hours.to_s.rjust(2, "0") + ":" + 
+		minutes.to_s.rjust(2, "0") + ":" + 
+		seconds.to_s.rjust(2, "0")
+	end
 end
