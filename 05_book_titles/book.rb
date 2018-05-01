0bf314e57fec1eeb6720a986247579245f605419
Eric Lambert <ericlamberttwo@gmail.com>
2018-05-01 13:10:28 -0700

    completed

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+	def title=(text)
+		@title = text.titleize
+	end 
+
+	def title
+		@title
+	end
+end
+
+class String
+	def titleize
+		smallWords = ["the", "and", "in", "of", "a", "an"]
+		words = self.split(" ").map { |word|
+			if smallWords.include? word
+				word.downcase
+			else
+				word.capitalize
+			end
+		}
+		words[0].capitalize!
+		words.join(" ")
+
+	end
 end
